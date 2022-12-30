6fb71289221d19b7e77d15158e99c5d8b214ccb7
Olivia <oliviatmanalastas@gmail.com>
2022-12-30 13:07:09 -0800

    conditional example

---
+++ day_3.py
@@ -0,0 +1,16 @@
+# Control Flow and Logical Operations
+
+# if condition:
+#  do this
+# else:
+#  do this
+
+water_level = 50
+if water_level > 80:
+    print("Drain Water")
+else:
+    print("Continue")
+
+# Checkout draw.io
+
+
