824258a295882f9683efafa059fa5ba5c321b6c0
Olivia <oliviatmanalastas@gmail.com>
2022-12-29 17:33:57 -0800

    ex 1 done

--- day1.py
+++ day1.py
@@ -1,1 +1,7 @@
-print('Hello World')
+#  print() function
+print('Hello World!')
+
+# Interactice Code Challenge (Printing)
+print('Day 1- Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')")
