c9b1869c00a5b1ac40e998e5f4cd04028f4e1374
Olivia <oliviatmanalastas@gmail.com>
2022-12-29 17:40:49 -0800

    string concat

--- day1.py
+++ day1.py
@@ -5,3 +5,9 @@ print('Hello World!')
 print('Day 1- Python Print Function')
 print('The function is declared like this:')
 print("print('what to print')")
+
+# Printing 3 times
+print('Hello World!\nHello World!\nHello World')
+
+# Concatinating two strings
+print('Hello' + '' + 'Molly')
