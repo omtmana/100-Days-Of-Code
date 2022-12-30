a464b8f745c38d4eee8fefd2b9243ee9379bfbc8
Olivia <oliviatmanalastas@gmail.com>
2022-12-30 13:29:28 -0800

    rollercoaster ride

--- day_3.py
+++ day_3.py
@@ -13,4 +13,12 @@ else:
 
 # Checkout draw.io
 
+# You need to be 120 cm to ride
+# Condition if you can or cannot ride
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
 
+if height >= 120:
+   print("You can ride the rollercoaster!")
+else:
+   print("You can't ride the rollercoaster")
