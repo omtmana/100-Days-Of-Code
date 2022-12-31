34a4aa5ad57160b16e1ad289bb39d089635d8dd4
Olivia <oliviatmanalastas@gmail.com>
2022-12-31 11:46:32 -0800

    mini challenge BMI2

--- day_3.py
+++ day_3.py
@@ -81,3 +81,20 @@ if height >= 120:
       print("Please pay $12")
 else:
    print("Sorry, you have to grow taller before you can ride.")
+
+# Checking for BMI
+height = float(input("What is your height in m? "))
+weight = float(input("What is your height in km? "))
+
+bmi = round(weight / (height ** 2))
+
+if bmi < 18.5:
+   print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+   print(f"Your BMI is {bmi}, you are a normal weight.")
+elif bmi < 30:
+   print(f"Your BMI is {bmi}, you are obese.")
+elif bmi < 35:
+   print(f"Your BMI is {bmi}, you are obese")
+else:
+   print(f"Your BMI is {bmi}, you are clinically obese")
