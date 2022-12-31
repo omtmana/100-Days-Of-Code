8578dc20f922f63b7966a1760de002b2eec7ad0d
Olivia <oliviatmanalastas@gmail.com>
2022-12-31 11:17:28 -0800

    Nested if statement example

--- day_3.py
+++ day_3.py
@@ -42,3 +42,15 @@ else:
 
 
 # We need to check their height and also age
+print("Welcome to the Rollercoaster")
+height = int(input("What is your height in cm"))
+
+if height >= 120:
+   print("You can ride the rollercoaster")
+   age = int(input("What is your age?"))
+   if age <= 18:
+      print("Please pay $7.")
+   else:
+      print("Please pay $12.")
+else:
+   print("Sorry you have to grow taller before you can ride.")
