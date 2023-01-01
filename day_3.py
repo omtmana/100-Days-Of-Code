ad941b676229645996f0c8b8b5969be020469f4b
Olivia <oliviatmanalastas@gmail.com>
2023-01-01 14:17:50 -0800

    solution for leap year checker added

--- day_3.py
+++ day_3.py
@@ -121,4 +121,15 @@ else:
     print(f"Your BMI is {bmi}, you are underweight.")
 
 # Leap year or Not leap year
+year = int(input("What year do you want to check?"))
 
+if year % 4 == 0:
+   if year % 100 == 0:
+      if year % 400 == 0:
+         print("Leap Year")
+      else:
+         print("Not leap year")
+   else:
+      print("Leap Year")
+else:
+   print("Not leap year")
