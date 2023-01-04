0377acf4f39aee2c7444c9b29cc38cfc629a78db
Olivia <oliviatmanalastas@gmail.com>
2023-01-04 10:12:56 -0800

    random

---
+++ day_4.py
@@ -0,0 +1,16 @@
+import random
+# Random
+# askpython.com
+# import random 
+
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+random_float = random.random()
+print(random_float)
+
+random_int_float = random_float * 5
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
