f30ed68f484dedf0c292bb180787cb8d939d48e7
Olivia <oliviatmanalastas@gmail.com>
2023-01-04 10:19:15 -0800

    heads or tails exercise

--- day_4.py
+++ day_4.py
@@ -14,3 +14,11 @@ random_int_float = random_float * 5
 
 love_score = random.randint(1, 100)
 print(f"Your love score is {love_score}")
+
+# Heads or Tails
+zero_one = random.randint(0,1)
+
+if zero_one == 1:
+   print("Heads")
+else:
+   print("Tails")
