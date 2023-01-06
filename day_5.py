f77308d8aed7586b4d6b7dd8719c7b0ff8f4a7fd
Olivia <oliviatmanalastas@gmail.com>
2023-01-06 11:30:22 -0500

    For Loop example

--- day_5.py
+++ day_5.py
@@ -3,3 +3,11 @@
 # For Loop
 for item in list_of_items:
    # Do something to each item
+
+fruits = ["Apple", "Peach", "Pear"]
+
+for fruit in fruits:
+   print(fruit)
+
+for fruit in 'Apple':
+   print(fruit)
