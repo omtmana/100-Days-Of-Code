0074abf28fbded06cd76e92ae0ec22f03f67eaec
Olivia <oliviatmanalastas@gmail.com>
2023-01-07 14:18:55 -0500

    Find average height using for in

--- day_5.py
+++ day_5.py
@@ -4,6 +4,7 @@
 for item in list_of_items:
    # Do something to each item
 
+# A list with three items
 fruits = ["Apple", "Peach", "Pear"]
 
 for fruit in fruits:
@@ -13,3 +14,15 @@ for fruit in fruits:
    print(fruit)
    print(fruit + "Pie")
    print(fruits)
+
+# Average Height
+# Find the average. The goal is to not use len() and sum()
+
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+# Write your code below this row 👇
