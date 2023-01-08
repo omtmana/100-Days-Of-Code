1fc3c6647e8e4afc5db3145f950e855f474be810
Olivia <oliviatmanalastas@gmail.com>
2023-01-08 10:39:52 -0500

    another solution for even total

--- day_5.py
+++ day_5.py
@@ -80,3 +80,9 @@ total = 0
 for number in range(2, 101, 2):
     total += number
 print(total)
+
+total2 = 0
+for number in range(1, 101):
+    if number % 2 == 0:
+        total2 += number
+print(total2)
