daf532e4f5a5a0b4baf3065b42a9479a0142a414
Olivia <oliviatmanalastas@gmail.com>
2023-01-10 17:04:45 -0500

    Final Solution Posted

--- day_6.py
+++ day_6.py
@@ -51,5 +51,16 @@ while condition_is_true:
    # Do this
 
 # Final Challenge for Day 6
+while front_is_clear:
+   move()
 
+turn_left()
 
+while not at_goal():
+   if right_is_clear():
+      turn_right()
+      move()
+   elif front_is_clear():
+      move()
+   else:
+      turn_left()
