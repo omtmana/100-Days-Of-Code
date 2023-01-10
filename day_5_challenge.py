4fbde394da9db3c5646803891fe72ee3323e807b
Olivia <oliviatmanalastas@gmail.com>
2023-01-10 12:51:40 -0500

    Added solution to password generator easy

--- day_5_challenge.py
+++ day_5_challenge.py
@@ -10,3 +10,19 @@ print("Welcome to the PyPassword Generator!")
 nr_letters = int(input(f"How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symsbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Eazy Level - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+password = ""
+# nr_letters
+for char in range(1, nr_letters + 1):
+   password += random.choice(letters)
+# nr_symbols
+for symb in range(1, nr_symbols + 1):
+   password += random.choice(symbols)
+
+# nr_numbers
+for num in range(1, nr_numbers + 1):
+   password += random.choice(numbers)
+
+print(password)
