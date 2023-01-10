d236eae9ff570e752ff3eb74335948e9518f68ae
Olivia <oliviatmanalastas@gmail.com>
2023-01-10 13:49:59 -0500

    Created functions for readable code

--- day_6.py
+++ day_6.py
@@ -8,3 +8,22 @@ def my_function_name():
 
 # Calling Functions
 my_function_name()
+
+# Functions are usefull because if you repeat
+# instructions it can carry out the steps over and over again
+
+# Reborgs World
+def turn_left():
+   # do this
+
+def move():
+   # do this
+
+def turn_right():
+   turn_left()
+   turn_left()
+   turn_left()
+
+
+
+
