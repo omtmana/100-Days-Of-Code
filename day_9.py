0b39dcdf8a2e79e756cd52983579e65b71fb5c5e
Olivia <oliviatmanalastas@gmail.com>
2023-01-18 10:09:37 -0500

    Python Dictionaries aka Objects in JS

--- day_9.py
+++ day_9.py
@@ -1,1 +1,10 @@
-# Dictionaries and Nesting
+# Dictionaries and Nesting (list inside list)
+
+# The Python Dictionaries
+# Allow us to group together and tag related pieces of information
+# Table: Key = Value e.g Loop = The action of doing something over and over again
+{key: Value}
+{
+   "Bug": "An error in a program that prevents the program form running as expected",
+   "print": "To print a message in Python"
+}
