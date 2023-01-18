aad950ce9845183fd854fec22afb60009c63fe4f
Olivia <oliviatmanalastas@gmail.com>
2023-01-18 10:12:08 -0500

    How to retrieve a value in a dictionary

--- day_9.py
+++ day_9.py
@@ -8,3 +8,14 @@
    "Bug": "An error in a program that prevents the program form running as expected",
    "print": "To print a message in Python"
 }
+
+programming_dictionary = {
+   "Bug": "Something",
+   "Function": "A piece of code that you can call over again",
+   "Loop": "Action of doing something over and over again"
+}
+
+# Retrive something from a dictionary
+programming_dictionary["Bug"]
+print(programming_dictionary["Bug"])
+dictionary_name["keyName"]
