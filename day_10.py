46aa999ea2a76eb33834992d1c994bd684aa4091
Olivia <oliviatmanalastas@gmail.com>
2023-01-23 09:27:06 -0500

    Functions with outputs. Return keyword

--- day_10.py
+++ day_10.py
@@ -1,1 +1,23 @@
-# Happy Chinese New Year
+# Day 10: Functions with outputs
+
+# Recap
+# Functions
+def my_function():
+   # Do this
+   # Then this
+
+my_function()
+
+# Functions with Inputs with something as parameter
+def my_function(something):
+   # Do this with something
+   # Then do this
+
+my_function(123) # 123 is an argument to parameter something
+
+# Now => Functions with outputs
+# return keyword
+def my_function():
+   return 2 * 3
+
+output = my_function():
