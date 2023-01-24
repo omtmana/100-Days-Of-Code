c0830ad9c1afbd05be00090625c6ccd1ea12d496
Olivia <oliviatmanalastas@gmail.com>
2023-01-24 14:36:39 -0500

    Added completed version of function with output

--- day_10.py
+++ day_10.py
@@ -26,4 +26,39 @@ output = my_function():
 def my_function(first_name, last_name):
    title_first_name = first_name.title()
    title_last_name = last_name.title()
-   return "{title_first_name} {title_last_name}"
+   return f"{title_first_name} {title_last_name}"
+
+# Completed Version
+# Functions with Outputs
+
+
+def format_name(f_name, l_name):
+  if f_name == "" or l_name == "":
+    return "You didn't provide valid inputs."
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  f"Result: {formated_f_name} {formated_l_name}"
+
+
+# Storing output in a variable
+formatted_name = format_name(
+    input("Your first name: "), input("Your last name: "))
+print(formatted_name)
+# or printing output directly
+print(format_name(input("What is your first name? "),
+      input("What is your last name? ")))
+
+# Already used functions with outputs.
+length = len(formatted_name)
+
+# Return as an early exit
+
+
+def format_name(f_name, l_name):
+  """Take a first and last name and format it 
+  to return the title case version of the name."""
+  if f_name == "" or l_name == "":
+    return "You didn't provide valid inputs."
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  return f"Result: {formated_f_name} {formated_l_name}"
